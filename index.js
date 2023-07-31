7e5654925cc6f4325764524cbeb27e0a7805f801
NA-Asraful-Khan <soyonshikhor@gmail.com>
2023-07-31 21:22:17 +0600

    first commit

--- index.js
+++ index.js
@@ -9,6 +9,13 @@ require('dotenv').config();
 const PORT = process.env.PORT||8000
 //middlewares
 app.use(cors());
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    next();
+  })
 app.use(express.urlencoded({extended:true}))
 app.use(express.json());
 
