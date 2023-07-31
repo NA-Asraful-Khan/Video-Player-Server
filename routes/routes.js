c0a6b48546e0efaffa9af07eff3eeedb428486e9
NA-Asraful-Khan <soyonshikhor@gmail.com>
2023-07-31 20:59:15 +0600

    first commit

---
+++ routes/routes.js
@@ -0,0 +1,11 @@
+const { addVideo, getAllVideos } = require('../controllers/videoController')
+const { videoUpload } = require('../middlewares/videoUpload')
+
+const router = require('express').Router()
+
+
+router
+.post('/upload',videoUpload.single('video'), addVideo)
+.get('/videos', getAllVideos)
+
+module.exports = router
